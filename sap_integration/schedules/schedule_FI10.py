a455a79e3c51001421fb492aec563d9a5c399fa2
Mohd Noor Shuhailey <mshuhaileyfx@gmail.com>
2024-12-12 23:54:23 +0800

    refactor: update timezone scheduler

--- sap_integration/schedules/schedule_FI10.py
+++ sap_integration/schedules/schedule_FI10.py
@@ -1,6 +1,6 @@
 from dagster import schedule
 from sap_integration.jobs import generate_FI10_and_push_flatfile_job
 
-@schedule(cron_schedule="0 * * * *", job=generate_FI10_and_push_flatfile_job, execution_timezone="UTC")
+@schedule(cron_schedule="0 * * * *", job=generate_FI10_and_push_flatfile_job, execution_timezone="Asia/Kuala_Lumpur")
 def schedules_FI10(_context):
     return {}
